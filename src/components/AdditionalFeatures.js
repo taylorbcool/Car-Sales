15258ffcd49602ca34d299dfc68ec47262694764
Taylor Cool <taycool@gmail.com>
2019-12-11 15:30:01 -0800

    finished mvp

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
+import { connect } from 'react-redux'
+import { addFeature } from '../actions/featuresActions'
 
 const AdditionalFeatures = props => {
   return (
@@ -8,7 +10,7 @@ const AdditionalFeatures = props => {
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} feature={item} addFeature={props.addFeature} />
           ))}
         </ol>
       ) : (
@@ -18,4 +20,10 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = state => {
+  return {
+    additionalFeatures: state.additionalFeatures
+  }
+}
+
+export default connect(mapStateToProps, { addFeature })(AdditionalFeatures);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { createStore } from 'redux';
+import { featuresReducer } from './reducers/featuresReducer'
+import { Provider } from 'react-redux'
+
+const store = createStore(featuresReducer)
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
